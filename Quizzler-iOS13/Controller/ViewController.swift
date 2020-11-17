eb770870be869f2abf71c786d89ee86e131e19cd
Jihun Park <jxxparker@gmail.com>
2020-11-17 11:28:33 -0600

    complete complete

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -6,6 +6,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
     
     var quizBrain = QuizBrain()
     
@@ -25,7 +26,7 @@ class ViewController: UIViewController {
             sender.backgroundColor = UIColor.red //changing the color to red if its wrong
         }
         
-        quizBrain.nextQuestion()
+        quizBrain.nextQuestion() //struct  from quizBrain from models
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         
     }
@@ -37,6 +38,7 @@ class ViewController: UIViewController {
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
         
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
         
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+![Screen Shot 2020-11-17 at 11 25 10 AM](https://user-images.githubusercontent.com/29707177/99424663-9185cc80-28c7-11eb-9428-fc26a5d1fa63.png)
+
+This is an application that allows you to answer a question with True or False answers.
+If answer is correct you will get a score at top right.
+If you finish the quiz you will be starting over and score will go down to 0.
+
+This app was built with basic logic of swift programming with MVC design pattern to make the code look cleaner.
